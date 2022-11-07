29d507f7ac0c9dfda40269df1026442cac4b95e2
pedroygor <pedroygor2010@hotmail.com>
2022-11-06 23:41:18 -0300

    feat: add new route, GET - /talker/:id

--- src/routes/talkerRoutes.js
+++ src/routes/talkerRoutes.js
@@ -5,7 +5,18 @@ const route = express.Router();
 
 route.get('/', async (request, response) => {
   const talkers = await getTalkerJson();
-  response.status(200).json(talkers);
+  return response.status(200).json(talkers);
+});
+
+route.get('/:id', async (request, response) => {
+  const { id } = request.params;
+  const talkers = await getTalkerJson();
+  const talker = talkers.find((item) => item.id === Number(id));
+  if (!talker) {
+    return response.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+  }
+
+  return response.status(200).json(talker);
 });
 
 module.exports = { route };
